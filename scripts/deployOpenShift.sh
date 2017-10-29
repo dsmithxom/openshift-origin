df4409bfd75fa6d9b02a7a14faa205ee1cf4a367
ivmckinl@microsoft.com <ivmckinl@microsoft.com>
2017-10-29 18:30:59 +0100

    Added fix to ensure nodeScript only takes first device returned. If a node has more disks added on provisioining then the script will fail as a list of items is returned instead of the expected one

--- scripts/deployOpenShift.sh
+++ scripts/deployOpenShift.sh
@@ -225,7 +225,8 @@ EOF
 
 for (( c=0; c<$GLUSTERCOUNT; c++ ))
 do
-devicename=$(runuser -l $SUDOUSER -c "ssh  $GLUSTER-$c sudo parted -m /dev/sda print all 2>/dev/null | grep unknown | grep /dev/sd | cut -d':' -f1  | awk 'NR==1'")
+devicename=$(runuser -l $SUDOUSER -c "ssh -i ~/.ssh/id_rsa $GLUSTER-$c sudo parted -m /dev/sda print all 2>/dev/null | grep unknown | grep /dev/sd | cut -d':' -f1  | awk 'NR==1'")
+echo "ARGGGG $devicename"
 echo "$GLUSTER-$c  glusterfs_devices='[ \"$devicename\" ]' " >> /etc/ansible/hosts
 done
 
