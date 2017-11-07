8f3ff5de16e03ceb7bc300af0d45672b01ac3810
ivmckinl@microsoft.com <ivmckinl@microsoft.com>
2017-11-07 19:26:09 +0100

    added notes

--- scripts/installinfo.sh
+++ scripts/installinfo.sh
@@ -2,3 +2,7 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
 yum check-update
 sudo yum install azure-cli
+
+
+
+az cloud register -n AzureStackUser --endpoint-resource-manager "https://management.local.azurestack.external"  --suffix-storage-endpoint "local.azurestack.external"  --suffix-keyvault-dns ".vault.local.azurestack.external"  --endpoint-active-directory-graph-resource-id "https://graph.windows.net/"  --endpoint-vm-image-alias-doc "https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/arm-compute/quickstart-templates/aliases.json"
