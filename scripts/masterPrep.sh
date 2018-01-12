30bfce04efcb0f66722b3576d3e59d1ab286a32e
ivmckinl@microsoft.com <ivmckinl@microsoft.com>
2018-01-12 18:54:02 +0100

    added  python-passlib to master prep

--- scripts/masterPrep.sh
+++ scripts/masterPrep.sh
@@ -22,7 +22,7 @@ sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
 if hostname -f|grep -- "-0" >/dev/null
 then
    echo $(date) " - Installing Ansible and pyOpenSSL"
-   yum -y --enablerepo=epel install ansible pyOpenSSL
+   yum -y --enablerepo=epel install ansible pyOpenSSL python-passlib
 fi
 
 ## install java
